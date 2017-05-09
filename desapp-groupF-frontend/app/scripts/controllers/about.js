2bb2d3e86365be569b9f2ed031b35a30cb64c421
sennedjem <sebastianariell@hotmail.com.ar>
2017-05-09 19:08:06 -0300

    Sitio angular

---
+++ desapp-groupF-frontend/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name desappGroupFFrontendApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the desappGroupFFrontendApp
+ */
+angular.module('desappGroupFFrontendApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ desapp-groupF-frontend/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name desappGroupFFrontendApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the desappGroupFFrontendApp
+ */
+angular.module('desappGroupFFrontendApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
