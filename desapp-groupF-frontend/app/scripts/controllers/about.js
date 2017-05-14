38a968c273660cab7abb44452aebc6396643132d
rodrigoep94 <rpapadopulo.2106@gmail.com>
2017-05-14 18:16:38 -0300

    Aplicación Front arreglada

---
+++ desapp-groupF-frontend/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name comprandoAloLocoApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the comprandoAloLocoApp
+ */
+angular.module('comprandoAloLocoApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ desapp-groupF-frontend/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name comprandoAloLocoApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the comprandoAloLocoApp
+ */
+angular.module('comprandoAloLocoApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
