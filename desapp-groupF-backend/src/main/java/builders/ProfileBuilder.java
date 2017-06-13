35343c7d8a3f3dfef2165b030dd935110a1837e1
sennedjem <sebastianariell@hotmail.com.ar>
2017-06-13 14:17:43 -0300

    profile builder

---
+++ desapp-groupF-backend/src/main/java/builders/ProfileBuilder.java
@@ -0,0 +1,45 @@
+package builders;
+
+import model.Address;
+import model.Profile;
+import utils.Money;
+
+public class ProfileBuilder {
+
+	private Profile profile;
+	
+	public ProfileBuilder(){
+		profile = new Profile();
+	}
+	
+	public void setProfile(Profile profile){
+		this.profile = profile;
+	}
+	
+	public Profile getProfile(){
+		return profile;
+	}
+	
+	public ProfileBuilder withMaxAmount(Money maxAmount){
+		this.profile.setMaxAmount(maxAmount);
+		return this;
+	}
+	
+	public ProfileBuilder withId(Integer id){
+		this.profile.setId(id);
+		return this;
+	}
+	
+	public ProfileBuilder withAddress(Address address){
+		this.profile.setDirection(address);
+		return this;
+	}
+	
+	public Profile build(){
+		Profile result = getProfile();
+		this.profile = new Profile();
+		return result;
+	}
+
+
+}
